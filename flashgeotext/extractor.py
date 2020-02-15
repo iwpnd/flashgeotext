ce4515bad87ca77d9cdc9b3c161fbc0f6239762c
iwpnd <legionaerr@googlemail.com>
2020-02-15 12:08:00 +0100

    added DemoData class

--- flashgeotext/extractor.py
+++ flashgeotext/extractor.py
@@ -1,4 +1,10 @@
+import json
+from typing import Union
+
 from flashtext import KeywordProcessor
+
+from flashgeotext.settings import DEMODATA_CITIES
+from flashgeotext.settings import DEMODATA_COUNTRIES
 
 
 class Alphabets(object):
@@ -6,11 +12,29 @@ class Alphabets(object):
 
 
 class DemoData(object):
-    cities: list = []
-    countries: list = []
+    cities: Union[list, dict] = []
+    countries: Union[list, dict] = []
 
-    def __init__(self):
-        pass
+    def __init__(self, with_synonyms: bool = True):
+        self.load_demo_data(with_synonyms=with_synonyms)
+
+    def load_demo_data(self, with_synonyms: bool = True) -> None:
+
+        if with_synonyms:
+            self.cities = self._load_data_dict(file=DEMODATA_CITIES)
+            self.countries = self._load_data_dict(file=DEMODATA_COUNTRIES)
+
+        else:
+            self.cities = self._load_data_list(file=DEMODATA_CITIES)
+            self.countries = self._load_data_list(file=DEMODATA_COUNTRIES)
+
+    def _load_data_dict(self, file: str = "") -> dict:
+        with open(file, "r", encoding="utf-8") as f:
+            return json.loads(f.read())
+
+    def _load_data_list(self, file: str = "") -> list:
+        with open(file, "r", encoding="utf-8") as f:
+            return list(json.loads(f.read()).keys())
 
 
 class Extractor(object):
