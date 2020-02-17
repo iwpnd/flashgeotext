9cb47eec4d9a9c4e44c198d4147bc47f276adbcb
iwpnd <legionaerr@googlemail.com>
2020-02-17 11:53:56 +0100

    added _flush_processor method on init to make sure to have an empty keyword processor. added tests

--- flashgeotext/extractor.py
+++ flashgeotext/extractor.py
@@ -14,23 +14,24 @@ class Extractor(object):
     cities_processor: KeywordProcessor = KeywordProcessor(case_sensitive=True)
     countries_processor: KeywordProcessor = KeywordProcessor(case_sensitive=True)
 
+
+class GeoLookup(Extractor):
+    cities: dict = {}
+    countries: dict = {}
+
     def __init__(self, use_demo_data: bool = True):
+        self._flush_processor()
+
         if use_demo_data:
             self.cities = load_data_from_file(file=DEMODATA_CITIES)
             self.countries = load_data_from_file(file=DEMODATA_COUNTRIES)
             self.build_cities_processor()
             self.build_countries_processor()
 
-    def build_cities_processor(self):
-        raise NotImplementedError
-
-    def build_countries_processor(self):
-        raise NotImplementedError
-
-
-class GeoLookup(Extractor):
-    cities: dict = {}
-    countries: dict = {}
+    def build(self):
+        self._flush_processor()
+        self.build_cities_processor()
+        self.build_countries_processor()
 
     def build_cities_processor(self):
         if self.cities:
@@ -39,6 +40,12 @@ class GeoLookup(Extractor):
     def build_countries_processor(self):
         if self.countries:
             self.countries_processor.add_keywords_from_dict(self.countries)
+
+    def _flush_processor(self):
+        self.cities_processor.keyword_trie_dict = dict()
+        self.cities_processor._terms_in_trie = 0
+        self.countries_processor.keyword_trie_dict = dict()
+        self.countries_processor._terms_in_trie = 0
 
 
 def load_data_from_file(file: str) -> dict:
--- tests/unit/test_extractor.py
+++ tests/unit/test_extractor.py
@@ -4,21 +4,53 @@ from flashgeotext.extractor import Extractor
 from flashgeotext.extractor import GeoLookup
 
 
-def test_extractor_import():
-    with pytest.raises(NotImplementedError):
-        ext = Extractor()
-        assert ext
+def test_extractor_init():
+    ext = Extractor()
+
+    assert hasattr(ext, "cities_processor")
+    assert hasattr(ext, "countries_processor")
 
 
-def test_lookupdata_content():
-    lookupdata = GeoLookup()
+def test_geolookup_demo_data():
+    geolookup_with_demodata = GeoLookup(use_demo_data=True)
 
-    assert hasattr(lookupdata, "cities")
-    assert hasattr(lookupdata, "countries")
+    assert geolookup_with_demodata.cities
+    assert geolookup_with_demodata.cities_processor
+    assert geolookup_with_demodata.countries
+    assert geolookup_with_demodata.countries_processor
 
 
-def test_lookupdata_load_data():
-    lookupdata = GeoLookup()
+def test_geolookup_no_demo_data_and_empty_processor():
+    geolookup_empty = GeoLookup(use_demo_data=False)
 
-    assert lookupdata.cities
-    assert lookupdata.countries
+    assert not geolookup_empty.cities
+    assert not geolookup_empty.countries
+    assert len(geolookup_empty.countries_processor) == 0
+    assert len(geolookup_empty.cities_processor) == 0
+
+
+@pytest.mark.parametrize(
+    "cities, countries, len_countries_processor, len_cities_processor",
+    [
+        (
+            {
+                "Berlin": ["Berlin", "german capitol"],
+                "Kopenhagen": ["Kopenhagen", "danish capitol"],
+            },
+            {"Germany": ["Germany"], "Denmark": ["Denmark"]},
+            2,
+            4,
+        )
+    ],
+)
+def test_geolookup_manual_data(
+    cities, countries, len_countries_processor, len_cities_processor
+):
+    geolookup = GeoLookup()
+    geolookup.cities = cities
+    geolookup.countries = countries
+
+    geolookup.build()
+
+    assert len(geolookup.cities_processor) == len_cities_processor
+    assert len(geolookup.countries_processor) == len_countries_processor
