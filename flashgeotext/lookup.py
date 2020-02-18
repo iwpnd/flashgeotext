4c84dc8f60e88ec7d1d605767eb8ac8ec3b92793
iwpnd <legionaerr@googlemail.com>
2020-02-18 13:28:05 +0100

    added remove method for lookupdataprocessor and tests

--- flashgeotext/lookup.py
+++ flashgeotext/lookup.py
@@ -1,4 +1,5 @@
 from flashtext import KeywordProcessor
+from loguru import logger
 from pydantic import BaseModel
 
 
@@ -18,11 +19,22 @@ class LookupDataProcessor:
     def __init__(self) -> None:
         self.pool: dict = {}
 
-    def add(self, lookup: LookupData, update: bool = False):
+    def add(self, lookup: LookupData, update: bool = False) -> None:
         if lookup.name in self.pool and not update:
             raise LookupDuplicateError(
-                f"{lookup.name} has already been added. Set update=True to update"
+                f"'{lookup.name}' has already been added. Set update=True to update"
             )
         else:
             self.pool[lookup.name] = KeywordProcessor(case_sensitive=True)
             self.pool[lookup.name].add_keywords_from_dict(lookup.data)
+            logger.debug(f"{lookup.name} added to pool")
+
+    def remove(self, lookup_to_remove: str) -> None:
+        if lookup_to_remove in self.pool:
+            del self.pool[lookup_to_remove]
+            logger.debug(f"{lookup_to_remove} removed from pool")
+
+
+class GeoText(LookupDataProcessor):
+    def __init__(self):
+        pass
--- tests/unit/test_dataprocessor.py
+++ tests/unit/test_dataprocessor.py
@@ -49,3 +49,25 @@ def test_lookup_data_processor_pool_duplicate_data(test_data_cities):
 
     with pytest.raises(LookupDuplicateError):
         processor.add(lookup)
+
+
+def test_lookup_data_processor_pool_duplicate_data_update_true(test_data_cities):
+    lookup = LookupData(name="cities", data=test_data_cities)
+
+    processor = LookupDataProcessor()
+    processor.add(lookup=lookup, update=True)
+    processor.add(lookup=lookup, update=True)
+
+    assert processor.pool["cities"]
+
+
+def test_lookup_data_processor_remove_lookup_from_pool(test_data_cities):
+    lookup = LookupData(name="cities", data=test_data_cities)
+
+    processor = LookupDataProcessor()
+    processor.add(lookup)
+
+    processor.remove(lookup_to_remove="cities")
+
+    with pytest.raises(KeyError):
+        assert processor.pool["cities"]
