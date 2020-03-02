394f3f12e77a5fbf2329eefb96b16bf82f527e1e
iwpnd <legionaerr@googlemail.com>
2020-03-02 14:22:29 +0100

    added scripts.json and added SCRIPTS to settings.py that is a dictionary of sc ripts.json

--- flashgeotext/settings.py
+++ flashgeotext/settings.py
@@ -1,6 +1,10 @@
+import json
 import os
 
 PARENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 DEMODATA_DIR = os.path.join(PARENT_DIRECTORY, "resources")
 DEMODATA_CITIES = DEMODATA_DIR + "/cities.json"
 DEMODATA_COUNTRIES = DEMODATA_DIR + "/countries.json"
+
+with open(DEMODATA_DIR + "/scripts.json", "r", encoding="utf-8") as f:
+    SCRIPTS = json.loads(f.read())
